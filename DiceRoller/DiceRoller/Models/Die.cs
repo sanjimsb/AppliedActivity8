78c75e40be40ef4ca502468bedddf38a3066ae3a
Bipin MSB <bpmsb45@gmail.com>
2022-08-09 20:10:15 -0400

    Created the DiceRoller project and DiceRollerTests Project - Create the DiceRoller project and created a Die class within the Models directory inside the DiceRoller project, included 3 data fields namely number of sides, type/name/colour and current side up. We also created a DieRollerTests class within the DiceRollerTests project within that class we have added the DieNullTest method which checks if there is class named Die, We used Test Driven Development and wrote a unit testing, We added the Fluent Assertions Nuget Package on the project named DiceRollerTests

---
+++ DiceRoller/DiceRoller/Models/Die.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DiceRoller
+{
+    public class Die
+    {
+        // name, type, or color of the die
+        public String Name { get; set; }
+
+        // Number of sides
+        public int NumSides { get; set; }
+
+        // which number is curren
+        public int CurrentSide { get; set; }
+    }
+}
+
---
+++ DiceRollerTests/DieRollerTests.cs
@@ -0,0 +1,18 @@
+﻿using DiceRoller;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiceRollerTests
+{
+    [TestClass]
+    public class DieRollerTests
+    {
+        [TestMethod]
+        public void DieNullTest()
+        {
+            Die d = new Die();
+            d.Should().NotBeNull();
+        }
+    }
+}
+
