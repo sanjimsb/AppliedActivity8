5ffc8bdc9b0ee3129e7b31335ac4acb56708f6da
Bipin MSB <bpmsb45@gmail.com>
2022-08-14 23:31:52 -0400

    Created a new project named UITest - Created a new project named UITest to test the UI of DiceRoller app, created a method named WelcomeTestIsDisplayed which tests if the text lable has the text as 'Welcome to Xamarin.Forms'

---
+++ UITest/AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+                return ConfigureApp
+                    .Android
+                    .InstalledApp("com.msb.diceroller")
+                    // TODO: Update this path to point to your Android app and uncomment the
+                    // code if the app is not included in the solution.
+                    //.ApkFile ("../../../Droid/bin/Debug/xamarinforms.apk")
+                    .StartApp();
+
+        }
+    }
+}
